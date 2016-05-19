8ea71801fbdd2dbe9c9d9b5713fa152019f4a025
dreamingblackcat <dreamingblackcat10@gmail.com>
2016-05-19 22:08:42 +0630

    Not flexible(only works with 9 element arrays due to type signature)  merge sort implementation

--- src/merge_sort.rs
+++ src/merge_sort.rs
@@ -1,16 +1,19 @@
 
 
-fn merge(mut arr: [u32;9], low: usize, mid: usize, high: usize) -> [u32;9]{
+fn merge(mut arr: &mut [u32;9], low: usize, mid: usize, high: usize) {
   let mut aux: [u32;9] = [0;9];
   let mut j: usize;
   let mut k: usize;
   j = low;
   k = mid + 1; 
-  for i in low..high+1 {
-     aux[i] = arr[i];
-  }
 
-  for i in low..high+1 {
+  for i in 0..arr.len() {
+      aux[i] = arr[i];
+  }
+  println!("before {:?}", arr);
+
+  for i in low..high +1 {
+    println!("{0}:{1}:{2}", i, j, k);
     if j > mid {
       arr[i] = aux[k];
       k += 1; 
@@ -25,17 +28,40 @@ fn merge(mut arr: [u32;9], low: usize, mid: usize, high: usize) -> [u32;9]{
       k += 1;
     }
   }
-  return arr;
+  println!("return {:?}", arr);
+  return;
+}
+
+fn merge_sort(mut arr: &mut [u32;9], low: usize, high: usize) {
+  if high <= low {
+    println!("Loop end here with high {0} and low {1}", high, low);
+    return;
+  }
+  let mid:usize = low + (high - low) / 2;
+  println!("mid {0}:{1}:{2}", mid, low, high);
+  merge_sort(&mut arr, low, mid);
+  merge_sort(&mut arr, mid+1, high);
+  return merge(&mut arr, low, mid, high);  
+
 }
 
 fn merge_test() {
-  let ary: [u32; 9] = [3, 2, 9, 7, 1, 8, 4, 6, 5];
-  let sorted: [u32; 9] = merge(ary, 0, 4, 8);
-  assert_eq!([3, 2, 8, 4, 6, 5, 9, 7, 1], sorted);
+  let mut ary: [u32;9] = [2, 3, 7, 9, 1, 4, 5, 6, 8];
+  merge(&mut ary, 0, 3, 8);
+  assert_eq!([1, 2, 3, 4, 5, 6, 7, 8, 9], ary);
+  
+}
+
+fn merge_sort_test() {
+  let mut ary: [u32;9] = [3, 2, 9, 7, 1, 8, 4, 6, 5];
+  let h = ary.len() as usize;
+  merge_sort(&mut ary, 0usize, h-1);
+  assert_eq!([1, 2, 3, 4, 5, 6, 7, 8, 9], ary);
   println!("Test Passed!");
 }
 
 
 fn main() {
   merge_test();
+  merge_sort_test();
 }
