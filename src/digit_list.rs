9ec723f891bd1ff76ea2396c2a3dbec459bf9781
dreamingblackcat <dreamingblackcat10@gmail.com>
2016-05-24 20:23:28 +0630

    Implement digit list addition

--- src/digit_list.rs
+++ src/digit_list.rs
@@ -1,6 +1,7 @@
 fn main() {
   let num = 100;
   assert_eq!(to_digit_list(num), [1, 0, 0]);
+  assert_eq!(add_dl(to_digit_list(num), to_digit_list(10)), [1,1,0]);
   println!("test passed!");
 }
 
@@ -14,4 +15,30 @@ fn to_digit_list(num: u32) -> Vec<u32> {
   }
 
   return list;
+}
+
+fn add_dl(first: Vec<u32>, second: Vec<u32>) -> Vec<u32> {
+  let mut result: Vec<u32> = vec![];
+  let mut first_cursor: usize = first.len();
+  let mut second_cursor: usize = second.len();
+  let mut carry: u32 = 0;
+
+  while first_cursor != 0 || second_cursor != 0 {
+    let mut sum:   u32 = carry;
+    if first_cursor != 0 {
+      sum += first[first_cursor - 1];
+      first_cursor -= 1;
+    }
+    if second_cursor != 0 {
+      sum += second[second_cursor - 1];
+      second_cursor -= 1;
+    }
+
+    if sum > 9 {
+      sum = sum % 10;
+      carry = sum / 10;
+    }
+    result.insert(0, sum);
+  }
+  return result;
 }
