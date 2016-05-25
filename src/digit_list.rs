69f4054295ac05955546179d46570b638afe951e
dreamingblackcat <dreamingblackcat10@gmail.com>
2016-05-25 23:30:04 +0630

    Implement digit list subtraction and comparison

--- src/digit_list.rs
+++ src/digit_list.rs
@@ -2,6 +2,10 @@ fn main() {
   let num = 100;
   assert_eq!(to_digit_list(num), [1, 0, 0]);
   assert_eq!(add_dl(to_digit_list(num), to_digit_list(10)), [1,1,0]);
+  assert_eq!(compare_dl(&to_digit_list(num), &to_digit_list(10)), 1);
+  assert_eq!(compare_dl(&to_digit_list(num), &to_digit_list(1000)), -1);
+  assert_eq!(compare_dl(&to_digit_list(num), &to_digit_list(100)), 0);
+  assert_eq!(subtract_dl(to_digit_list(num), to_digit_list(10)), [9,0]);
   println!("test passed!");
 }
 
@@ -39,6 +43,74 @@ fn add_dl(first: Vec<u32>, second: Vec<u32>) -> Vec<u32> {
       carry = sum / 10;
     }
     result.insert(0, sum);
+  }
+  return result;
+}
+
+fn compare_dl(first: &Vec<u32>, second: &Vec<u32>) -> i32 {
+  let mut res: i32 = 0;
+  if first.len() > second.len() {
+    res = 1;
+  } else if first.len() < second.len(){
+    res = -1;
+  } else {
+    let mut cursor: usize = 0;
+
+    while cursor != 0 || cursor != 0 {
+      if first[cursor] < second[cursor] {
+        res = -1;
+        break;
+      } else if first[cursor] > second[cursor] {
+        res = 1;
+        break;
+      }
+      cursor += 1;
+    }
+  }
+  return res;
+}
+
+fn subtract_dl(first: Vec<u32>, second: Vec<u32>) -> Vec<u32> {
+  let mut result: Vec<u32> = vec![];
+  let mut first_cursor: usize = first.len();
+  let mut second_cursor: usize = second.len();
+  let mut borrowed: bool = false;
+  
+  if compare_dl(&first, &second) == -1 {
+    panic!("First operand must be greater than or equal to second operand");
+  }
+
+  while first_cursor != 0  {
+    let mut res: u32 = first[first_cursor - 1];
+    if borrowed {
+      res -= 1;
+    }
+
+
+    if second_cursor == 0 {
+      borrowed = false;
+    }
+
+    if second_cursor > 0 && res < second[second_cursor - 1] {
+      borrowed = true;
+      res += 10;
+      res -= second[second_cursor - 1];
+      second_cursor -= 1;
+    }
+
+    if second_cursor > 0  && res >= second[second_cursor - 1] {
+      res -= second[second_cursor - 1];
+      second_cursor -= 1;
+      borrowed = false;
+    }
+
+    
+    first_cursor  -= 1;
+    if first_cursor == 0 && res == 0{
+      break;
+    } else {
+      result.insert(0, res);
+    }
   }
   return result;
 }
